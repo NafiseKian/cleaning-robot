e72e2779002c3c4d77bb2114e13171a499e6ec4e
Nnamchisimi <mosimiloluwannamchi@gmail.com>
2024-04-17 18:55:17 +0300

    pls check script and main

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <python.h>
+#include <iostream>
+
+
+int main(int argc,char*argv[])
+{
+    Py_Initialize()
+    PyObject *name, *load_module,*func,*callfunc,*args;
+    name=PyUnicode_FromString((char*)"script");
+    load_module=PyImport_Import(name);
+    func = PyObject_GetAttrString(load_module,(char*)"fun1");
+    callfunc=PyObject_CallObject(func,NULL);
+    double fun1_out=PyFloat_AsDouble(callfunc);
+
+    func = PyObject_GetAttrString(load_module,(char*)"fun2");
+    args = PyTuple_Pack(1,PyFloat_FromDouble(4.0))
+    callfunc=PyObject_CallObject(func,NULL);
+    double fun2_out=PyFloat_AsDouble(callfunc);
+    
+    py_Finalize();
+    std::cout <<fun1_out<<std::endl;
+    std::cout <<fun2_out<<std::endl;
+
+
+    g++ main.cpp -o output -L C:\Users\mosim\AppData\Local\Programs\Python\Python37-32\libs -lpython37 -I C:\Users\mosim\AppData\Local\Programs\Python\Python37-32\include
+
+
+}
---
+++ script.py
@@ -0,0 +1,6 @@
+def fun1():
+    print (" first function")
+    return 2*2
+def fun2(x):
+    print("second function")
+    return x/2 
