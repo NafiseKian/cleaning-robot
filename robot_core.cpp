2d9474d92f0ce6f737a85521aeefdcb32c87e4a9
NafiseKian <nafiseyousefikian@gmail.com>
2024-04-22 22:19:31 +0300

    wifi module updated

--- robot_core.cpp
+++ robot_core.cpp
@@ -10,11 +10,37 @@
 #include "camera_module.h"
 #include "drive_module.h"
 #include "ultra_sensor.h"
+#include "wifi_module.h"
 
 int main() 
 {
+    // Initialize motor control
+    MotorControl::setup();
+    std::cout <<"motor controller set up done"<< std::endl;
+
     int photoCounter = 0;
+
     GPSModule gps;
+
+    std::vector<Localization::AccessPoint> access_points = {
+      {1.0, 1.0, "0A:96:71:47:4C:FF"}, // JN extension
+      {5.0, 1.0, "F6:43:35:08:ED:BD"},  // JN
+      {3.0, 5.0, "2A:F4:8D:C2:36:E5"}   // recordreaker
+    };
+
+    Localization wifi_loc(access_points);
+
+    // Get current position based on measured distances from WiFi packets
+    std::pair<double, double> estimated_position = wifi_loc.getCurrentPositionFromWiFi();
+
+    if (!std::isnan(estimated_position.first)) {
+        std::cout << "Estimated Robot Position (x, y): " << estimated_position.first << ", " << estimated_position.second << std::endl;
+    } else {
+        std::cerr << "Error: Robot location estimation failed." << std::endl;
+    }
+    
+
+
     //UltrasonicSensor sensor1 = UltrasonicSensor();
 
     while(true)
@@ -31,9 +57,7 @@ int main()
             std::cout << "No GPS data available yet." << std::endl;
         }
 
-        // Initialize motor control
-        MotorControl::setup();
-        std::cout <<"set up done"<< std::endl;
+        
         // Move forward for 2 seconds
         MotorControl::forward();
         std::cout <<"called forward"<< std::endl;
@@ -42,8 +66,11 @@ int main()
 
         // Capture a photo
         CameraModule::capturePhoto(photoCounter);
+        
+        //call object detection
+        //call arm module to pick the trash 
 
-        if(photoCounter==3) break ; 
+        if(photoCounter==2) break ; 
     }
 
     return 0;
