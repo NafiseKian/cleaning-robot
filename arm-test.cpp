c193dff6a22392ec6a9ba17952ebfc0bc28186ce
jeries-123 <119804522+jeries-123@users.noreply.github.com>
2024-06-16 12:46:49 +0300

    test 3

--- arm-test.cpp
+++ arm-test.cpp
@@ -29,15 +29,16 @@ int main() {
     gpioSetMode(servo3_pin, PI_OUTPUT);
 
     while (true) {
+        // Set servo1 (pin 2) to 180 degrees and servo2 (pin 3) to 0 degrees
+        setServoAngle(servo1_pin, 180);
+        setServoAngle(servo2_pin, 0);
+
+        // Sweep servo3 (pin 4) back and forth
         for (int pos = 0; pos <= 180; pos++) {
-            setServoAngle(servo1_pin, pos);
-            setServoAngle(servo2_pin, pos);
             setServoAngle(servo3_pin, pos);
             usleep(15000); // 15 ms
         }
         for (int pos = 180; pos >= 0; pos--) {
-            setServoAngle(servo1_pin, pos);
-            setServoAngle(servo2_pin, pos);
             setServoAngle(servo3_pin, pos);
             usleep(15000); // 15 ms
         }
