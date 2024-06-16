bcf55d4441349e50b89c79b9bba68603f6eef1ed
jeries-123 <119804522+jeries-123@users.noreply.github.com>
2024-06-16 12:40:10 +0300

    test 2

--- arm-test.cpp
+++ arm-test.cpp
@@ -1,30 +1,48 @@
-#include <Servo.h>
+#include <pigpio.h>
+#include <iostream>
+#include <unistd.h>
 
-Servo servo1;  // create servo object to control the right hand
-Servo servo2;  // create servo object to control the left hand
-Servo servo3;  // create servo object to control the third servo
+const int servo1_pin = 2; // GPIO 2
+const int servo2_pin = 3; // GPIO 3
+const int servo3_pin = 4; // GPIO 4
 
-int servo1_pin = 2; // right hand
-int servo2_pin = 3; // left hand
-int servo3_pin = 4; // third servo
+const int MIN_PULSE_WIDTH = 500;
+const int MAX_PULSE_WIDTH = 2500;
 
-void setup() {
-  servo1.attach(servo1_pin);  // attaches the servo on pin 2 to the servo object
-  servo2.attach(servo2_pin);  // attaches the servo on pin 3 to the servo object
-  servo3.attach(servo3_pin);  // attaches the servo on pin 4 to the servo object
+int angleToPulseWidth(int angle) {
+    return (MIN_PULSE_WIDTH + (angle * (MAX_PULSE_WIDTH - MIN_PULSE_WIDTH) / 180));
 }
 
-void loop() {
-  for (int pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees in steps of 1 degree
-    servo1.write(pos);  // tell servo to go to position in variable 'pos'
-    servo2.write(pos);  // tell servo to go to position in variable 'pos'
-    servo3.write(pos);  // tell servo to go to position in variable 'pos'
-    delay(15);          // waits 15ms for the servo to reach the position
-  }
-  for (int pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    servo1.write(pos);  // tell servo to go to position in variable 'pos'
-    servo2.write(pos);  // tell servo to go to position in variable 'pos'
-    servo3.write(pos);  // tell servo to go to position in variable 'pos'
-    delay(15);          // waits 15ms for the servo to reach the position
-  }
+void setServoAngle(int gpio, int angle) {
+    int pulseWidth = angleToPulseWidth(angle);
+    gpioServo(gpio, pulseWidth);
+}
+
+int main() {
+    if (gpioInitialise() < 0) {
+        std::cerr << "pigpio initialisation failed." << std::endl;
+        return 1;
+    }
+
+    gpioSetMode(servo1_pin, PI_OUTPUT);
+    gpioSetMode(servo2_pin, PI_OUTPUT);
+    gpioSetMode(servo3_pin, PI_OUTPUT);
+
+    while (true) {
+        for (int pos = 0; pos <= 180; pos++) {
+            setServoAngle(servo1_pin, pos);
+            setServoAngle(servo2_pin, pos);
+            setServoAngle(servo3_pin, pos);
+            usleep(15000); // 15 ms
+        }
+        for (int pos = 180; pos >= 0; pos--) {
+            setServoAngle(servo1_pin, pos);
+            setServoAngle(servo2_pin, pos);
+            setServoAngle(servo3_pin, pos);
+            usleep(15000); // 15 ms
+        }
+    }
+
+    gpioTerminate();
+    return 0;
 }
