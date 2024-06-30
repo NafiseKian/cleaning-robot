ef89fcf73bdd8e74fb8ac375cf339553dd542144
jeries123 <jeriest59@gmail.com>
2024-06-30 14:18:09 +0300

    arduino-interface

---
+++ test-in.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <wiringSerial.h>
+#include <unistd.h> // for usleep
+
+int main() {
+    int fd;
+    char command = 'a';
+
+    if ((fd = serialOpen("/dev/ttyACM0", 9600)) < 0) { // Open serial port
+        std::cerr << "Unable to open serial device." << std::endl;
+        return 1;
+    }
+
+    usleep(2000000); // Wait for 2 seconds for serial to stabilize
+
+    // Send command to Arduino
+    serialPutchar(fd, command);
+
+    usleep(100000); // Wait for data to be transmitted
+
+    serialClose(fd); // Close serial port
+    return 0;
+}
